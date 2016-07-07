2688822bd25bfa61506c9ea81f55a1826f067cf1
dgoff96 <dennis@thelendesigngroup.com>
2016-07-07 15:23:11 -0400

    randomize color list

--- js/SPECTACLES.js
+++ js/SPECTACLES.js
@@ -1079,11 +1079,33 @@ var SPECTACLES = function (divToBind, jsonFileData, callback) {
 
         return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
     };
+    
+    
+    //shuffle a list
+    function shuffle(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+    }
 
     //Color Code by attribute
     SPECT.colorByAttribute = function(){
         var numColors = SPECT.attributeList.length;
-        var colorList = rgbColors(numColors);
+        var colors = rgbColors(numColors);
+        var colorList = shuffle(colors);
         //console.log(colorList);
 //        console.log(colorList.length);
 //        console.log(numColors);
